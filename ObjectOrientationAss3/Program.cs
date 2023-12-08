d31794b5244ed60478056cf8246374cddd252c59
GuardianEatos97 <5100158@mypillar7uwc.ac.za>
2023-12-08 16:31:07 +0200

    First Commit

---
+++ ObjectOrientationAss3/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ObjectOrientationAss3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TvSpecs TV1 = new TvSpecs("Samsung", 50, 150, true, "Fior", "Black", false) ;
+
+            Console.WriteLine($"The brand of the Tv is {TV1.BrandOfTv} and is {TV1.WidthOfTv}cm wide and {TV1.HeightOfTv}cm tall. The tv {TV1.Resolution}.");
+            TV1.PowerButton();
+        }
+    }
+}
---
+++ ObjectOrientationAss3/TvSpecs.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientationAss3
+{
+    internal class TvSpecs
+    {
+        public string BrandOfTv;
+        public int WidthOfTv;
+        public int HeightOfTv;
+        public string Resolution;
+        string ColorOfTv;
+        public bool Power;
+        SpeakersSpecs SpeakersSpecs;
+
+        public TvSpecs(string brandoftv, int widthOfTv, int heightOfTv, bool resolution, string speakername, string coloroftv, bool power)
+        {
+            WidthOfTv = widthOfTv;
+            HeightOfTv = heightOfTv;
+            BrandOfTv = brandoftv;
+            ColorOfTv = coloroftv;
+            Power = power;
+            SpeakersSpecs = new SpeakersSpecs();
+            SpeakersSpecs.Name = speakername;
+
+            if (resolution) 
+            {
+                Resolution = "is a high resolution tv";
+            }
+            else
+            {
+                Resolution = "is not a high resolution tv";
+            }
+        }
+        public bool PowerButton()
+        {
+            return Power = !Power;
+        }
+    }
+
+    
+}
